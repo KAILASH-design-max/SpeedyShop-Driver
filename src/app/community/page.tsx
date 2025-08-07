451fa61cce4d19ff419b9ae293a3bfeae6e937e4
KAILASH KUMAR AMAN 22215065 <kailash.kumar@bca.christuniversity.in>
2025-08-07 05:22:20 +0000

    To remove the gap from the header of the "Community & Announcements" car

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -5,7 +5,7 @@ import { Announcements } from "@/components/community/Announcements";
 
 export default function CommunityPage() {
   return (
-    <div className="container mx-auto p-6 h-[calc(100vh-6.5rem)]">
+    <div className="container mx-auto px-6 pb-6 h-[calc(100vh-6.5rem)]">
         <Announcements />
     </div>
   );
