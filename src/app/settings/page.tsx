07b19b6d47c2d7230ac43786239f0b7594b9db99
KAILASH KUMAR AMAN 22215065 <kailash.kumar@bca.christuniversity.in>
2025-08-07 05:26:28 +0000

    To remove the gap from the header of the "Settings" card. 0 4px; /* Very

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -3,7 +3,7 @@ import { SettingsPage } from "@/components/settings/SettingsPage";
 
 export default function Settings() {
   return (
-    <div className="px-1 py-6">
+    <div className="px-1 pb-6">
         <SettingsPage />
     </div>
   );
