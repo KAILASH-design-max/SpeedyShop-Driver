917c7812436309f1d3157bfde151bc8de5200407
KAILASH KUMAR AMAN 22215065 <kailash.kumar@bca.christuniversity.in>
2025-08-07 08:02:59 +0000

    To remove the gap (padding or margin) from the left and right sides of t

--- src/app/orders/[id]/page.tsx
+++ src/app/orders/[id]/page.tsx
@@ -309,7 +309,7 @@ export default function OrderPage() {
 
 
   return (
-    <div className="space-y-6 pb-6 px-6">
+    <div className="space-y-6 pb-6 px-1">
       <OrderDetailsDisplay order={order} />
 
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
