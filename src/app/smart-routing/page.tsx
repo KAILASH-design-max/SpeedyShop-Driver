139544810e76b99c370bd649b800ad99a0073613
KAILASH KUMAR AMAN 22215065 <kailash.kumar@bca.christuniversity.in>
2025-08-07 05:20:44 +0000

    To remove the gap from the header of the "Smart Route Optimizer" card. 0

--- src/app/smart-routing/page.tsx
+++ src/app/smart-routing/page.tsx
@@ -3,7 +3,7 @@ import { RouteOptimizer } from "@/components/routing/RouteOptimizer";
 
 export default function SmartRoutingPage() {
   return (
-    <div className="py-6 px-1">
+    <div className="pb-6 px-1">
       <RouteOptimizer />
     </div>
   );
