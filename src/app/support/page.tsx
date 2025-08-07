ebd5829afe30030bd2384343631978ee5c64325d
KAILASH KUMAR AMAN 22215065 <kailash.kumar@bca.christuniversity.in>
2025-08-07 05:27:44 +0000

    To remove the gap (padding or margin) from the left and right sides of t

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 export default function SupportPage() {
   return (
-    <div className="container mx-auto p-6">
+    <div className="px-1 py-6">
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
         <div className="lg:col-span-2 space-y-8">
           <Faq />
