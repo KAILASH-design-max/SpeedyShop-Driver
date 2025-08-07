852e89abb201e6453b0c38feb85befc3ab6f4714
KAILASH KUMAR AMAN 22215065 <kailash.kumar@bca.christuniversity.in>
2025-08-07 05:28:29 +0000

    To remove the gap from the header of the "Frequently Asked Questions" ca

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 export default function SupportPage() {
   return (
-    <div className="px-1 py-6">
+    <div className="px-1 pb-6">
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
         <div className="lg:col-span-2 space-y-8">
           <Faq />
