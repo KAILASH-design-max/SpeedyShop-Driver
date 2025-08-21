45c40aa6042037d350961e23b65e66229fa8a265
KAILASH KUMAR AMAN 22215065 <kailash.kumar@bca.christuniversity.in>
2025-08-21 17:08:47 +0000

    Application error: a client-side exception has occurred while loading sp

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 import { initializeApp, getApp, getApps, FirebaseApp } from "firebase/app";
 import { getAuth, Auth } from "firebase/auth";
 import { getFirestore, Firestore } from "firebase/firestore";
@@ -11,20 +10,19 @@ let storage: FirebaseStorage;
 
 if (typeof window !== "undefined") {
     // This code only runs on the client
-    const firebaseConfig = {
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    };
+    const firebaseConfigString = process.env.NEXT_PUBLIC_FIREBASE_WEBAPP_CONFIG;
+    if (!firebaseConfigString) {
+        throw new Error("Firebase config not found in environment variables");
+    }
     
+    const firebaseConfig = JSON.parse(firebaseConfigString);
+
     if (!getApps().length) {
         app = initializeApp(firebaseConfig);
     } else {
         app = getApp();
     }
+
     auth = getAuth(app);
     db = getFirestore(app);
     storage = getStorage(app);
