7906fe8f695374fc635087c3b8044f1c7c720f97
KAILASH KUMAR AMAN 22215065 <kailash.kumar@bca.christuniversity.in>
2025-11-11 17:10:24 +0000

    remove 1 heaer from this

--- src/app/settings/login-activity/layout.tsx
+++ src/app/settings/login-activity/layout.tsx
@@ -1,10 +1,8 @@
 
-import AuthenticatedLayout from '@/components/layout/AuthenticatedLayout';
-
 export default function LoginActivityLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <AuthenticatedLayout>{children}</AuthenticatedLayout>;
+  return <>{children}</>;
 }
