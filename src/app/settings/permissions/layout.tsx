e9303314e020e0a27139cfbd081d6e55897c58dc
KAILASH KUMAR AMAN 22215065 <kailash.kumar@bca.christuniversity.in>
2025-11-11 17:08:51 +0000

    remove 1 heaer from this

--- src/app/settings/permissions/layout.tsx
+++ src/app/settings/permissions/layout.tsx
@@ -1,10 +1,8 @@
 
-import AuthenticatedLayout from '@/components/layout/AuthenticatedLayout';
-
 export default function PermissionsSettingsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <AuthenticatedLayout>{children}</AuthenticatedLayout>;
+  return <>{children}</>;
 }
