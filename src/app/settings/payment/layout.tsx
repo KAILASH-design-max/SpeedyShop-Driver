fece2b2acfeacb1c9282b1cce2954ab212b6fea5
KAILASH KUMAR AMAN 22215065 <kailash.kumar@bca.christuniversity.in>
2025-11-12 04:31:51 +0000

    remove 1 header from this

--- src/app/settings/payment/layout.tsx
+++ src/app/settings/payment/layout.tsx
@@ -1,10 +1,8 @@
 
-import AuthenticatedLayout from '@/components/layout/AuthenticatedLayout';
-
 export default function PaymentSettingsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <AuthenticatedLayout>{children}</AuthenticatedLayout>;
+  return <>{children}</>;
 }
